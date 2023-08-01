2968573d9d796dfaae0b9f7b91292c8a66117cfe
Martin Jul <martin@mjul.com>
2023-08-01 14:31:44 +0200

    Expose a Rust struct to Node

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# neon-typescript-rust-lab
-Taking Neon for spin
+# Neon - Rust core with TypeScript scripting
+
+
+Contents:
+- [cpu-count](cpu-count/README.md) is the example from the Neon docs.
+- [allbooks](allbooks/README.md) is a more complex example with more complex Rust types.
+
+## Requirements
+- NPM 7 minimum 
+- Rust 
--- allbooks/src/lib.rs
+++ allbooks/src/lib.rs
@@ -1,11 +1,52 @@
 use neon::prelude::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+/// A Book
+/// Taken from https://neon-bindings.com/docs/objects
+struct Book {
+    pub title: String,
+    pub author: String,
+    pub year: u32,
 }
+
+impl Book {
+    /// Map this to JavaScript object.
+    ///
+    /// [Context<'a>] is a trait, so we can use it with  various contexts like [FunctionContext<'a>] and [ModuleContext<'a>].
+    fn to_object<'a>(&self, cx: &mut impl Context<'a>) -> JsResult<'a, JsObject> {
+        let obj = cx.empty_object();
+        let title = cx.string(&self.title);
+        obj.set(cx, "title", title)?;
+        let author = cx.string(&self.author);
+        obj.set(cx, "author", author)?;
+        let year = cx.number(self.year);
+        obj.set(cx, "year", year)?;
+        Ok(obj)
+    }
+}
+
+
+/// Main function, exposes this library to JavaScript.
+///
+/// Example:
+/// Run `node` in the `allbooks` directory after building with `npm install` and `npm run build --release`, then:
+///
+/// ```JavaScript
+///     const allbooks = require('.')
+///     allbooks.chadwick
+/// ```
+/// Output:
+/// ```JavaScript
+///    {
+///    title: 'Chadwick the Crab',
+///    author: 'Priscilla Cummings',
+///    year: 2009
+///   }
+/// ````
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    let book = Book { title: "Chadwick the Crab".to_string(), author: "Priscilla Cummings".to_string(), year: 2009 };
+    let obj = book.to_object(&mut cx)?;
+    cx.export_value("chadwick", obj)?;
     Ok(())
 }
